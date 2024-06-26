f8a6bc93a546bc1ac3b6540cbdd0d4be67c34168
Gregorio <Greg.del801@gmail.com>
2024-06-26 12:30:54 -0700

    completed lab

--- server/app.py
+++ server/app.py
@@ -20,15 +20,31 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
+# Define the route for listing all articles
 @app.route('/articles')
 def index_articles():
+    # Query all articles and convert them to dictionaries
 
-    pass
+    articles = [article.to_dict() for article in Article.query.all()]
+    #Return a JSON response with the list of articles
+    return make_response(jsonify(articles), 200)
 
-@app.route('/articles/<int:id>')
+# Define teh route for showing a single article
+@app.route('/articles/<int:id>', methods=['GET'])
 def show_article(id):
+    # Get 'page_views' from session, default to 0 if not set
+    # Use a ternary-like operation to initialize 'page_views' in session if not already set
+    session['page_views'] = session.get('page_views') or 0
+    # Increment 'page_views' by 1
+    session['page_views'] += 1
 
-    pass
+    # Check if the user has viewed 3 or fewer pages
+    if session['page_views'] <= 3: 
+        #Return the Article as a JSON
+        return Article.query.filter(Article.id == id).first().to_dict(), 200
+    #Return an error message if page view limit is exceeded
+    return {'message': 'Maximum pageview limit reached'}, 401 
+
 
 if __name__ == '__main__':
     app.run(port=5555)
